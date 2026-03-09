31b851d1f4682983f39c551e1b6f536c6a1f094f
PINKY228228 <131951507+PINKY228228@users.noreply.github.com>
2026-03-09 21:54:09 +0900

    Add files via upload

--- home/test_project/run_test.sh
+++ home/test_project/run_test.sh
@@ -1,61 +1,36 @@
 #!/bin/bash
+set -e
 
-# Windowsキー押す
-# powershell と入力
-# Windows PowerShell を起動
-# taskkill /F /IM bash.exe
-# taskkill /F /IM mintty.exe
+MODE=$1
 
-set -ex
+# 実際のテスト数を見る方法
+# ./build/tests/csv/csvTests.exe
+case "$MODE" in
+    csv)
+        ./tests/csv/run_csv_test.sh
+        ;;
+    unit)
+        ./tests/unit/run_unit_test.sh
+        ;;
+    coverage)
+        echo "=== run all tests for coverage ==="
 
-echo "start test"
+        ./tests/unit/run_unit_test.sh
+        ./tests/csv/run_csv_test.sh
 
-rm -rf build
-#mkdir build
+        echo "=== generate coverage ==="
 
-echo "configure"
-# echo "configure (Debug)"
-# VSCode拡張を入れる
-# ms-vscode.cpptools
-# setup-x86_64.exeでgdbインストール
-# -S .        → source (CMakeLists.txt の場所)
-# -B build    → buildディレクトリ
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
-#cmake ..
+        lcov --capture --directory . --output-file coverage.info
+        lcov --remove coverage.info '/usr/*' --output-file coverage.info
 
-echo "build"
-cmake --build build
-#make -j$(nproc)
-cd build
+        genhtml coverage.info --output-directory coverage_html
 
-echo "run test"
-ctest --output-on-failure
-
-echo "collect coverage"
-lcov --capture \
-     --directory . \
-     --output-file coverage.info \
-     --ignore-errors empty,inconsistent
-#lcov --capture --directory . --output-file coverage.info --ignore-errors empty
-
-echo "filter coverage"
-lcov --capture \
-     --directory . \
-     --output-file coverage.info \
-     --ignore-errors empty,inconsistent
-# lcov --remove coverage.info '/usr/*' '*/googletest/*' \
-#      --output-file coverage_filtered.info --ignore-errors empty
-
-echo "generate html"
-# 必要
-# 検索　TimeDate
-# perl-TimeDate
-# cd test_project2
-# cd build
-# ls *.info
-# coverage.info
-# genhtml coverage.info --output-directory coverage
-# C:\cygwin64\home\PC_User\test_project2\build\coverage
-# 配下にhtml生成される
-echo "coverage report:"
-echo "$(pwd)/coverage/index.html"
+        echo ""
+        echo "coverage html:"
+        echo "coverage_html/index.html"
+        ;;
+    *)
+        ./tests/unit/run_unit_test.sh
+        ./tests/csv/run_csv_test.sh
+        ;;
+esac
