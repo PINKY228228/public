7ab3161f481959d9ebdad207560281f2de5941c1
PINKY228228 <131951507+PINKY228228@users.noreply.github.com>
2026-03-17 21:13:07 +0900

    Add files via upload

--- home/test_project/tests/developer/Rte_mock.cc
+++ home/test_project/tests/developer/Rte_mock.cc
@@ -16,6 +16,13 @@ extern "C" void RteMock_SetVehicleSpeed(uint16_t v);
 /* ---- 内部モックデータ ---- */
 static uint16_t mock_vehicleSpeed = 0;
 static bool lamp_value;
+struct MockDID
+{
+    uint8_t did2800;
+    uint8_t did2801;
+};
+
+static MockDID mock;
 
 /* ---- RTE API モック ---- */
 extern "C" Std_ReturnType Rte_Read_VehicleSpeed_Value(uint16_t* speed)
@@ -23,6 +30,18 @@ extern "C" Std_ReturnType Rte_Read_VehicleSpeed_Value(uint16_t* speed)
     //*speed = gu16_vehicleSpeed;
     *speed = mock_vehicleSpeed;
     return RTE_E_OK;
+}
+
+extern "C" Std_ReturnType Rte_Read_DID2800(uint8_t* v)
+{
+    *v = mock.did2800;
+    return RTE_E_OK;    
+}
+
+extern "C" Std_ReturnType Rte_Read_DID2801(uint8_t* v)
+{
+    *v = mock.did2801;
+    return RTE_E_OK;    
 }
 
 /* ---- モック制御API ---- */
@@ -47,6 +66,13 @@ extern "C" void RteMock_SetVehicleSpeed(uint16_t v)
 {
     mock_vehicleSpeed = v;
 }
+
+
+void RteMock_SetDID(uint8_t v0, uint8_t v1)
+{
+    mock.did2800 = v0;
+    mock.did2801 = v1;
+}    
 
 extern "C" void Rte_Write_Lamp_Value(uint8_t v)
 {
