5c06b8766f9dc5377f27565083644c26fd08e151
PINKY228228 <131951507+PINKY228228@users.noreply.github.com>
2026-04-03 22:54:05 +0900

    Add files via upload

--- home/test_project/src/comp.h
+++ home/test_project/src/comp.h
@@ -3,8 +3,26 @@
 #ifndef COMP_H
 #define COMP_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*comp_func_t)(void);
+
+void set_comp_A(comp_func_t f);
+void set_comp_B(comp_func_t f);
+void comp_cycle(void);
+
+void comp_cycle2(void);
+void func_a(void);
+void func_b(void);
+
 void comp_cycle(void);
 void comp_A(void);
 void comp_B(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- home/test_project/src/comp_main.c
+++ home/test_project/src/comp_main.c
@@ -1,9 +1,33 @@
 // comp_main.c
 
-#include "comp.h"
 #include "stdio.h"
+#include "comp.h"
+#include "ControlLamp.h"
 
+static comp_func_t comp_A_impl = 0;
+static comp_func_t comp_B_impl = 0;
 
+void set_comp_A(comp_func_t f) { comp_A_impl = f; }
+void set_comp_B(comp_func_t f) { comp_B_impl = f; }
+
+void comp_cycle(void)
+{
+    if (comp_A_impl) comp_A_impl();
+    if (comp_B_impl) comp_B_impl();
+}
+
+void comp_cycle2(void)
+{
+    func_a();
+    App_ControlLamp();
+}
+
+void func_b(void)
+{
+
+}
+
+#if 0
 void comp_cycle(void)
 {
         printf("ENTER\n");
@@ -12,3 +36,4 @@ void comp_cycle(void)
     comp_A();
     comp_B();
 }
+#endif
